509f5b921001b6863de6e4ede1dc3ffb57b8a48e
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-06-29 21:21:37 +0300

    fixed view bug on phones

--- app.py
+++ app.py
@@ -1,151 +1,59 @@
-# Import the Flask class from the flask module
-from flask import Flask, render_template, request, redirect, url_for,flash
-from ini_database import Diseases_dict,Diseases_info,new_sy_list,chosen_symptoms
-
-
-# Create a new Flask app instance
-app = Flask(__name__)
-app.secret_key = 'iugegiujegehgdtijoie'
+<!-- Layout.html -->
+<!DOCTYPE html>
 
 
 
-# Function to format the symptoms entered by the user
-def format_symptoms(sy):
-    # Split the symptoms into a list
-    sy = sy.split(',')
-   
-    # Convert all symptoms to lowercase and remove any leading/trailing whitespace
-    sy = [i.lower().strip() for i in sy]
-    
-    return sy
-
-# Function to determine possible diagnoses based on entered symptoms
-def get_diseases(symptoms_list):
-    # Create a dictionary to count the number of times each disease appears
-    dict_count = {}
-    
-    # Convert all symptoms to a format that matches the keys in Diseases_dict
-    new_symptoms_list = [i.strip("']").strip("['").replace(" ","_") for i in symptoms_list]
-    
-    # Loop through all symptoms and diseases to count the number of matches
-    for sym in new_symptoms_list:
-        for key, value in Diseases_dict.items():
-            if sym in value:
-                try:
-                    dict_count[key] += 1
-                except Exception as e:
-                    dict_count[key] = 1
-            
-    # Create a dictionary that maps diseases to the percentage of symptoms matched
-    diseases_dict_percent = {i:get_percent(i,dict_count) for i in dict_count.keys()}
-
-    # Sort the diseases by the percentage of symptoms matched
-    sorted_diseases = sorted(diseases_dict_percent.items(), key=lambda x: int(dict_count[x[0]])/len(Diseases_dict[x[0]]), reverse=True)
-    
-    return sorted_diseases
-
-# Function to calculate the percentage of symptoms matched for a given disease
-def get_percent(disease,dict):
-    return f'Has {dict[disease]} {"symptom" if int(dict[disease])==1 else "symptoms" } out of {len(Diseases_dict[disease])} '
-
-# Define a route for the home page
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-# Define a route for the symptoms page
-@app.route('/symptoms', methods=['GET', 'POST'])
-def symptoms():
-    if request.method == "POST":
-        # Get the symptoms entered by the user and redirect to the diseases page
-        symptoms = request.form["sy"]
-        if len(symptoms)<1: # Used Inputted Empty List
-            flash("Cannot Be Empty!")
-            return render_template('symptoms.html')
-        else:
-            print(len(symptoms),symptoms)
-            return redirect(url_for("diseases",sy=symptoms))
-    else:
-        # Display the symptoms page
-        return render_template("symptoms.html")
-
-# Define a route for the diseases page
-@app.route('/diseases/<sy>')
-def diseases(sy):
-    # Format the symptoms entered by the user
-    symptoms = format_symptoms(sy)
-    print(symptoms)
-    for i in symptoms:
-        if len(i)<4:
-            flash("List Can't Be Empty :)")
-            print(i)
-            return redirect(url_for('choose'))
-        else:
-            print(len(i))
-            print("Else")
-            continue
-    
-    # Get possible diagnoses based on the entered symptoms
-    global diseases_final 
-    diseases_final = get_diseases(symptoms_list=symptoms) 
-
-    #remove squared brackets 
-    symptoms = [i.strip("['").strip("']") for i in symptoms]
-
-    # Render the diseases page with the list of possible diagnoses
-   
-    
-    return render_template('diseases.html',keys=diseases_final,sys=symptoms)
-
-# Define a route for the learn more page
-@app.route('/learnmore/<disease>',methods = ['GET','POST'])
-def learn_more(disease):
-    # Render the learn more page with information on the selected disease
-    return render_template('learn_more.html',info = Diseases_info[disease],name = disease)
-
-# Define a route for the symptoms set page
-@app.route('/symptoms_set')
-def symptoms_set():
-    # Convert the set of unique symptoms to a list and render the symptoms set page
-    sy_list = list(new_sy_list)
-    return render_template('symptoms_set.html',sys=sy_list)
-
-# Define a route for the about page
-@app.route('/about')
-def about():
-    return render_template('about.html',title = 'About')
-
-# Define a route for the choose page
-@app.route('/choose',methods=['GET','POST'])
-def choose():
-    # Sort the list of unique symptoms and render the choose page
-    sy_list = sorted(new_sy_list)
-    
-    return render_template('choose.html',chosen_list = chosen_symptoms,sys_list=sy_list)
+<html>
+  <head>
+    <title>Flask Yassin</title>
+    <link rel="shortcut icon" type="image/x-icon" href="https://i.postimg.cc/cLb8TySj/Logo-prev-ui.png" />
+    <link rel="icon" type="image/x-icon" href="https://i.postimg.cc/cLb8TySj/Logo-prev-ui.png" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href={{ url_for('static',filename='style.css') }} />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-@app.route('/add_symptom/<symptom>')
-def add_symptom(symptom):
-    
-    chosen_symptoms.append(symptom)
-    return redirect(url_for('choose'))
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  </head>
+  <body>
 
-@app.route('/call_diseases') 
-def call_diseases():
-    """Called Only from the choose.html page , to call the diseases function."""
-    global chosen_symptoms
-    return_value = redirect(url_for('diseases',sy=chosen_symptoms))
-    
-    chosen_symptoms = []
-    
-    return return_value
 
-# Define a route for the contact page
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    {% block flash_msg %}
+        <div class="container">
+            {% for message in get_flashed_messages() %}
+            <div class="alert alert-warning">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                {{ message }}
+            </div>
+            {% endfor %}
 
-# Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+            {% block page_content %}{% endblock %}
+        </div>
+    {% endblock %}
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="{{url_for('home')}}"> <img src="https://i.postimg.cc/sXFx1tzz/Diagnoz-Logo-removebg-preview.png" width="260" ></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+
+          </li>
+          <li class="nav-item">
+         <a class="nav-link" href="{{url_for('about')}}">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{url_for('contact')}}">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      {% block content %}
+      {% endblock %}
+    </div>
+  </body>
+</html>
