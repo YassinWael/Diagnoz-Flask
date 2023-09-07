e4b529da269616f1642362bae3fdd8fb05c57040
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-09-07 13:25:18 +0300

    Fixed Babel + Google Verification

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ from ini_database import Diseases_dict,Diseases_info,new_sy_list,chosen_symptoms
 # Create a new Flask app instance
 app = Flask(__name__)
 app.secret_key = 'iugegiujegehgdtijoie'
-
-#doing this as testing another feature
-try:
-    def get_locale():
-        return session.get('language', 'en')
-
-    babel = Babel(app,locale_selector=get_locale)
-except Exception as e:
-    pass
+babel = Babel(app)
+def get_locale():
+    return session.get('language', 'en')
+babel.init_app(app, locale_selector=get_locale)
 
 
 # Define a route for setting the language
