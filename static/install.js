29d30ee5698a177c1c7407a5695244c6aad5a8d6
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-09-09 00:32:45 +0300

    trying fixes

--- static/install.js
+++ static/install.js
@@ -1,22 +1,25 @@
-let deferredPrompt
-console.log("install runing")
-window.onload = Logic
+let deferredPrompt;
+
+console.log("install running");
+
+window.onload = Logic;
+
 function showPrompt() {
-    deferredPrompt.prompt(); 
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+  }
 }
 
 // Download Button
 function Logic() {
+  window.addEventListener('beforeinstallprompt', function(e) {
+    e.preventDefault();
+    deferredPrompt = e;
+    const installButton = document.getElementById('installButton');
+    installButton.style.display = 'block';
+  });
 
-window.addEventListener('beforeinstallprompt', function(e) {
-e.preventDefault();
-    deferredPrompt = e
-
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'none';
+  installButton.onclick = showPrompt;
 }
-)
-const install = document.getElementById('installButton');
-install.onclick = showPrompt;
-
-
-}
-
