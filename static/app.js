32d87bb3cb8ebf39ac4dd5fcf5e41adaac9a88d8
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-09-09 11:36:34 +0300

    download show only in navbar if available

--- static/app.js
+++ static/app.js
@@ -3,3 +3,8 @@ if (navigator.serviceWorker) {
     navigator.serviceWorker.register('static/service-worker.js')
 }
 
+window.addEventListener('beforeinstallprompt',function(e) {
+    downloadNav = document.getElementById('download')
+    downloadNav.style.display = 'block'
+})
+
