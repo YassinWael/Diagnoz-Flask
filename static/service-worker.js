92a9fbd5d7a3b39fb95368f2f99c63e77b652ae1
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-09-09 12:11:40 +0300

    updated service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,48 +1,22 @@
-// Function to handle the installation of the service worker
+console.log('Starting service worker');
 
-console.log('Starting service worker')
-function handleInstall(event) {
-    event.waitUntil(
-      openCache().then(function(cache) {
-        return cacheAddAll(cache, [
-          '/',
-          'templates/*',
-          'static/*',
-          'dataset/*'
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/*',
+        '/templates/*',
+        'static/*',
+        '/datasets/*'
+      ]);
+    })
+  );
+});
 
-        ]);
-      })
-    );
-  }
-  
-  // Function to open the cache
-  function openCache() {
-    return caches.open('my-cache');
-  }
-  
-  // Function to add multiple files to the cache
-  function cacheAddAll(cache, files) {
-    console.log(files)
-    return cache.addAll(files);
-  }
-  
-  // Function to handle the fetch event
-  function handleFetch(event) {
-    event.respondWith(
-      matchCache(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  }
-  
-  // Function to check if the requested resource is available in the cache
-  function matchCache(request) {
-    return caches.match(request);
-  }
-  
-  // Event listener for the 'install' event
-  self.addEventListener('install', handleInstall);
-  
-  // Event listener for the 'fetch' event
-  self.addEventListener('fetch', handleFetch);
-  
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
