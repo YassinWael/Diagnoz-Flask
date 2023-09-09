c14bf402da41f0aa59ad1e1500f589c72cb57736
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-09-09 14:30:54 +0300

    f

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,47 @@
-// Install the service worker
-self.addEventListener('install', function(event) {
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "home.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open('offline').then(function(cache) {
-      return cache.addAll([
-        '/',
-        "/template/about.html",
-"/template/choose.html",
-"/template/contact.html",
-"/template/diseases.html",
-"/template/download.html",
-"/template/home.html",
-"/template/layout.html",
-"/template/learn_more.html",
-"/template/symptoms.html",
-"/template/symptoms_set.html",
-"/datasets/dataset.csv",
-"/datasets/symptom_Description.csv",
-"/static/style.css",
-"/static/images/512Logo.png",
-"/static/images/apple-icon-180.png",
-"/static/images/download-app.png",
-"/static/images/manifest-icon-192.maskable.png",
-"/static/images/manifest-icon-512.maskable.png"
-      ]);
-    })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-// Fetch the content using the service worker
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
