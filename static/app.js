681e30df611c34cdb8512048ab9d63910182eecb
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-09-09 14:39:31 +0300

    v

--- static/app.js
+++ static/app.js
@@ -1,19 +1,4 @@
 
-// Ensure that the browser supports the service worker API
-if (navigator.serviceWorker) {
-    // Start registration process on every page load
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js')
-            // The register function takes as argument
-            // the file path to the worker's file
-            
-            // Gives us registration object
-            .then(reg => console.log('Service Worker Registered'))
-            .catch(swErr => console.log(
-                  `Service Worker Installation Error: ${swErr}}`));
-      });
-  }
-
 window.addEventListener('beforeinstallprompt',function(e) {
     downloadNav = document.getElementById('download')
     downloadNav.style.display = 'block'
