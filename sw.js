635012ec66d42463b0159b48738f4d20258748aa
YassinWael <101746515+YassinWael@users.noreply.github.com>
2023-09-10 20:43:22 +0300

    smaller bug

--- sw.js
+++ sw.js
@@ -25,25 +25,20 @@ if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+self.addEventListener('fetch', function(event) {
+    // Calling event.respondWith means we're in charge
+    // of providing the response. We pass in a promise
+    // that resolves with a response object
+    event.respondWith(
+      // First we look for something in the caches that
+      // matches the request
+      caches.match(event.request).then(function(response) {
+        // If we get something, we return it, otherwise
+        // it's null, and we'll pass the request to
+        // fetch, which will use the network.
+        return response || fetch(event.request);
+      })
+    );
+  });
 
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(cachedPages);
-        return cachedResp;
-      }
-    })());
-  }
-});
 }
